19cc81edf28c92f70a5960727688c6f124a6f534
ece-mohammad <kuro.ece@gmail.com>
2025-05-02 13:56:51 +0300

    Initial commit
    Signed-off-by: ece-mohammad <kuro.ece@gmail.com>

---
+++ src/twrap/cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A thin CLI wrapper around python `textwrap.fill` function to wrap text.
+
+"""
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+class TestCli:
+
+    def test_stub(self):
+        pass
+
+
+if __name__ == "__main__":
+    pytest.main()
